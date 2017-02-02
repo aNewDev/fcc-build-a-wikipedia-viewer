eae7e1d888024d832a72cfef33eb23fc005521e2
aNewDev <iam@anewdev.io>
2017-02-02 16:01:54 -0600

    Closes #2 Set Wikipedia Search
    Wikipedia search is now working on the page

--- scripts.js
+++ scripts.js
@@ -1,7 +1,52 @@
+// get data from form and trigger wikipedia search
+function getFormData(){
+  var search = document.getElementById('search').value;
+  getWikipediaData(search);
+}
 
+// Pull data from wikipedia api
+function getWikipediaData(search) {
+  var output = $.ajax({
+    url: "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&generator=search&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + search + "&gsrlimit=10&utf8=",
+    type: 'POST',
+    data: {},
+    dataType: 'json',
+    headers: { 'Api-User-Agent': 'CodePen Exercise/1.1 (https://codepen.io/anewdev/pen/dNmNKJ; iam@anewdev.io)' },
+    success: function(data) {
+      // if data pull successful, trigger getDetails function with the pulled data
+      displayResults(data.query.pages);
+      hideSearchBox();
+    },
+    error: function(err) { alert(err); },
+  });
+};
 
+// display the results
+
+function displayResults(object) {
+  var node = "";
+  for (var variable in object) {
+    var pageLink = "https://en.wikipedia.org/?curid=" + object[variable].pageid;
+    node += "<div class=" + "list-group" + "><a href=" + pageLink + " class=" + "list-group-item active" + " target=" + "_blank" + "><h4 class=" + "list-group-item-heading" + ">" + object[variable].title + "</h4><p class=" + "list-group-item-text" + ">" + object[variable].extract + "</p></a></div>";
+    document.getElementById("data").innerHTML = node;
+  };
+};
+
+function hideSearchBox() {
+  document.getElementById("searchBox").style.visibility = "hidden";
+  document.getElementById("random").style.visibility = "hidden";
+};
+
+function showSearchBox() {
+  document.getElementById("data").innerHTML = "";
+  document.getElementById("searchBox").style.visibility = "visible";
+  document.getElementById("random").style.visibility = "visible";
+
+};
 
 /*
+
+
 
 // Set global variables
 var weatherData = {};
