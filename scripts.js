d0694415b438e49d3dbe2e677ac52b98cc5d34f1
aNewDev <iam@anewdev.io>
2017-02-02 16:58:05 -0600

    Fixes #5, Closes #6 List View and Clear Search
    Fixed the list view to show correctly
    Clear search button does just that and returns to search again

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+$(document).ready(function() {
+  document.getElementById("clear").style.display = "none";
+});
+
+
 // get data from form and trigger wikipedia search
 function getFormData(){
   var search = document.getElementById('search').value;
@@ -27,71 +32,21 @@ function displayResults(object) {
   var node = "";
   for (var variable in object) {
     var pageLink = "https://en.wikipedia.org/?curid=" + object[variable].pageid;
-    node += "<div class=" + "list-group" + "><a href=" + pageLink + " class=" + "list-group-item active" + " target=" + "_blank" + "><h4 class=" + "list-group-item-heading" + ">" + object[variable].title + "</h4><p class=" + "list-group-item-text" + ">" + object[variable].extract + "</p></a></div>";
+    node += "<a href=" + pageLink + " class=" + "list-group-item active" + " target=" + "_blank" + "><h4 class=" + "list-group-item-heading" + ">" + object[variable].title + "</h4><p class=" + "list-group-item-text" + ">" + object[variable].extract + "</p></a>";
     document.getElementById("data").innerHTML = node;
   };
 };
 
 function hideSearchBox() {
-  document.getElementById("searchBox").style.visibility = "hidden";
-  document.getElementById("random").style.visibility = "hidden";
+  document.getElementById("searchBox").style.display = "none";
+  document.getElementById("random").style.display = "none";
+  document.getElementById("clear").style.display = "inline";
 };
 
 function showSearchBox() {
   document.getElementById("data").innerHTML = "";
-  document.getElementById("searchBox").style.visibility = "visible";
-  document.getElementById("random").style.visibility = "visible";
-
+  document.getElementById("searchBox").style.display = "inline";
+  document.getElementById('search').value = "";
+  document.getElementById("random").style.display = "inline";
+  document.getElementById("clear").style.display = "none";
 };
-
-/*
-
-
-
-// Set global variables
-var weatherData = {};
-
-// After loading the page, get lat lon from ipinfo and execute the first function
-$(document).ready(function() {
-  $.get("http://ipinfo.io/geo", function(response) {
-    var latLon = (response.loc).split(',');
-    var latitude = latLon[0];
-    var longitude = latLon[1];
-    document.getElementById("city").innerHTML = "The current weather in<br>" + response.city;
-    getWeatherDetail(latitude, longitude);
-  }, "jsonp")
-});
-
-// Pull the weather from openweathermap and post the weather on the page
-function getWeatherDetail(lat, lon) {
-  var output = $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&APPID=09449d6c9c3b937d6a67e55e34b1bfab",
-    type: 'GET',
-    data: {},
-    dataType: 'json',
-    success: function(data) {
-    // assign the weatherData for future use, and set the page values to current weather
-      weatherData = data;
-      changeBackgroundImg(weatherData.weather[0].main);
-      getFahrenheit();
-      document.getElementById("conditions").innerHTML = weatherData.weather[0].description;
-    },
-    error: function(err) { alert(err); },
-  });
-};
-
-// change the background image
-function changeBackgroundImg(group) {
-  document.body.style.backgroundImage = "url(https://s3.amazonaws.com/assetsanewdevio/fccweather/" + group + ".jpg)";
-}
-
-// get celsius temp from fahrenheit
-function getCelsius() {
-  document.getElementById("temp").innerHTML = Math.round((weatherData.main.temp - 32) * 5 / 9) + '° <a onclick="getFahrenheit()" href="#">C</span>';
-}
-
-// get fahrenheit from api data
-function getFahrenheit() {
-  document.getElementById("temp").innerHTML = Math.round(weatherData.main.temp) + '° <a onclick="getCelsius()" href="#">F</span>';
-}
-*/
